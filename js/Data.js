72dbd7ca671f8275ce9f5941d09db19e8718d0a7
nrdev <nelson_rojas_janda@hotmail.com>
2022-03-09 10:49:26 +0100

    Add data script

---
+++ js/Data.js
@@ -0,0 +1,19 @@
+class Data{
+    constructor(description, value){
+        this._ddescription = description;
+        this._value = value;
+    }
+
+    get description(){
+        return this._description
+    }
+    set description(description){
+        this._description = description
+    }
+    get value(){
+        return this._value
+    }
+    set value(value){
+        this._value = value
+    }
+}
